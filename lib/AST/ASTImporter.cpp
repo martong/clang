b55d54b333e117615d0e078fdc60f90950272b4d
Zoltán Gera <gerazo80@gmail.com>
2017-12-21 15:21:03 +0100

    Also handle empty body functions

--- lib/AST/ASTImporter.cpp
+++ lib/AST/ASTImporter.cpp
@@ -934,20 +934,22 @@ bool ASTNodeImporter::ImportDeclParts(NamedDecl *D, DeclContext *&DC,
   // Check if RecordDecl is in FunctionDecl parameters to avoid infinite loop.
   // example: int struct_in_proto(struct data_t{int a;int b;} *d);
   DeclContext *OrigDC = D->getDeclContext();
-  if(isa<RecordDecl>(D) && OrigDC->isFunctionOrMethod()) {
-    FunctionDecl *FunDecl = dyn_cast<FunctionDecl>(OrigDC);
-    if (FunDecl && FunDecl->hasBody()) {
+  FunctionDecl *FunDecl;
+  if (isa<RecordDecl>(D) && (FunDecl = dyn_cast<FunctionDecl>(OrigDC))) {
+    bool DoNotImport = !FunDecl->hasBody();
+    if (!DoNotImport) {
       SourceRange RecR = D->getSourceRange();
       SourceRange BodyR = FunDecl->getBody()->getSourceRange();
       // If RecordDecl is not in Body (it is a param), we bail out.
-      if (RecR.isValid() && BodyR.isValid() &&
-            (RecR.getBegin() < BodyR.getBegin() ||
-             BodyR.getEnd() < RecR.getEnd())) {
-        Importer.FromDiag(D->getLocation(), diag::err_unsupported_ast_node)
-              << D->getDeclKindName();
-        Importer.setEncounteredUnsupportedNode(true);
-        return true;
-      }
+      DoNotImport = RecR.isValid() && BodyR.isValid() &&
+                    (RecR.getBegin() < BodyR.getBegin() ||
+                     BodyR.getEnd() < RecR.getEnd());
+    }
+    if (DoNotImport) {
+      Importer.FromDiag(D->getLocation(), diag::err_unsupported_ast_node)
+          << D->getDeclKindName();
+      Importer.setEncounteredUnsupportedNode(true);
+      return true;
     }
   }
 
