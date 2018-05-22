4c781cd33f3c8b985f345c68e6ea180cf565dcee
Balázs Kéri <1.int32@gmail.com>
2018-05-22 14:38:29 +0200

    added ASTNodeImporter::VisitCXXStdInitializerListExpr

---
+++ test/ASTMerge/std-initializer-list/Inputs/il.cpp
@@ -0,0 +1,2 @@
+#include <initializer_list>
+std::initializer_list<int> IL = {1, 2, 3, 4};
---
+++ test/ASTMerge/std-initializer-list/test.cpp
@@ -0,0 +1,3 @@
+// RUN: %clang++ -x c++-header -std=c++11 -o %t.1.ast %S/Inputs/il.cpp
+// RUN: %clang_cc1 -x c++ -ast-merge %t.1.ast -fsyntax-only %s 2>&1 | FileCheck --allow-empty %s
+// CHECK-NOT: unsupported AST node
