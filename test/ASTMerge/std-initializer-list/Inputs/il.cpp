ba6e163f0797402e68a220461f0a4f2fc71b5bd7
Gyorgy Orban <gyorgy.orban@ericsson.com>
2018-06-07 08:00:10 +0200

    add initializer_list mock for testing
    if STL is missing on the host machine the tests
    will not run without a mock

--- test/ASTMerge/std-initializer-list/Inputs/il.cpp
+++ test/ASTMerge/std-initializer-list/Inputs/il.cpp
@@ -1,2 +1,8 @@
-#include <initializer_list>
+namespace std {
+  template<typename T> struct initializer_list {
+    const T *begin, *end;
+    initializer_list();
+  };
+}
+
 std::initializer_list<int> IL = {1, 2, 3, 4};
