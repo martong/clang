b09cf156e8d000b0b131520e1d58860d31d0f21d
Raphael Isemann <teemperor@gmail.com>
2018-08-20 16:20:01 +0000

    [ASTImporter] Add test for C++ casts and fix broken const_cast importing.
    Summary:
    The ASTImporter does currently not handle const_casts. This patch adds the
    missing const_cast importer code and the test case that discovered this.
    
    Reviewers: a.sidorin, a_sidorin
    
    Reviewed By: a_sidorin
    
    Subscribers: a_sidorin, martong, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D50932
    
    git-svn-id: https://llvm.org/svn/llvm-project/cfe/trunk@340182 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/AST/ASTImporter.cpp
+++ lib/AST/ASTImporter.cpp
@@ -6969,6 +6969,10 @@ Expr *ASTNodeImporter::VisitCXXNamedCastExpr(CXXNamedCastExpr *E) {
     return CXXReinterpretCastExpr::Create(
         Importer.getToContext(), ToType, VK, CK, ToOp, &BasePath,
         ToWritten, ToOperatorLoc, ToRParenLoc, ToAngleBrackets);
+  } else if (isa<CXXConstCastExpr>(E)) {
+    return CXXConstCastExpr::Create(Importer.getToContext(), ToType, VK, ToOp,
+                                    ToWritten, ToOperatorLoc, ToRParenLoc,
+                                    ToAngleBrackets);
   } else {
     return nullptr;
   }
