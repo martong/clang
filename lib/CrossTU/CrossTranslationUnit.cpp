25b0bf2dd320ff399b238bf1c6564dee43e0285d
Gabor Marton <gabor.marton@ericsson.com>
2018-09-21 17:04:03 +0200

    Add triple/lang mismatch handling

--- lib/CrossTU/CrossTranslationUnit.cpp
+++ lib/CrossTU/CrossTranslationUnit.cpp
@@ -29,6 +29,36 @@
 #include <fstream>
 #include <sstream>
 
+namespace llvm {
+// Same as Triple's equality operator, but we check a field only if that is
+// known in both instances.
+bool hasEqualKnownFields(const Triple &Lhs, const Triple &Rhs) {
+  return ((Lhs.getArch() != Triple::UnknownArch &&
+           Rhs.getArch() != Triple::UnknownArch)
+              ? Lhs.getArch() == Rhs.getArch()
+              : true) &&
+         ((Lhs.getSubArch() != Triple::NoSubArch &&
+           Rhs.getSubArch() != Triple::NoSubArch)
+              ? Lhs.getSubArch() == Rhs.getSubArch()
+              : true) &&
+         ((Lhs.getVendor() != Triple::UnknownVendor &&
+           Rhs.getVendor() != Triple::UnknownVendor)
+              ? Lhs.getVendor() == Rhs.getVendor()
+              : true) &&
+         ((Lhs.getOS() != Triple::UnknownOS && Rhs.getOS() != Triple::UnknownOS)
+              ? Lhs.getOS() == Rhs.getOS()
+              : true) &&
+         ((Lhs.getEnvironment() != Triple::UnknownEnvironment &&
+           Rhs.getEnvironment() != Triple::UnknownEnvironment)
+              ? Lhs.getEnvironment() == Rhs.getEnvironment()
+              : true) &&
+         ((Lhs.getObjectFormat() != Triple::UnknownObjectFormat &&
+           Rhs.getObjectFormat() != Triple::UnknownObjectFormat)
+              ? Lhs.getObjectFormat() == Rhs.getObjectFormat()
+              : true);
+}
+}
+
 namespace clang {
 namespace cross_tu {
 
@@ -41,6 +71,8 @@ STATISTIC(
     "The # of getCTUDefinition called but the function is not in other TU");
 STATISTIC(NumGetCTUSuccess, "The # of getCTUDefinition successfully return the "
                             "requested function's body");
+STATISTIC(NumTripleMismatch, "The # of triple mismatches");
+STATISTIC(NumLangMismatch, "The # of language mismatches");
 
 // FIXME: This class is will be removed after the transition to llvm::Error.
 class IndexErrorCategory : public std::error_category {
@@ -65,6 +97,10 @@ public:
       return "Failed to load external AST source.";
     case index_error_code::failed_to_generate_usr:
       return "Failed to generate USR.";
+    case index_error_code::triple_mismatch:
+      return "Triple mismatch";
+    case index_error_code::lang_mismatch:
+      return "Language mismatch";
     }
     llvm_unreachable("Unrecognized index_error_code.");
   }
@@ -180,6 +216,29 @@ CrossTranslationUnitContext::getCrossTUDefinition(const FunctionDecl *FD,
   }
   assert(&Unit->getFileManager() ==
          &Unit->getASTContext().getSourceManager().getFileManager());
+  const auto& TripleTo = Context.getTargetInfo().getTriple();
+  const auto& TripleFrom = Unit->getASTContext().getTargetInfo().getTriple();
+  // The imported AST had been generated for a different target
+  // TODO use equality operator. Note, for some unknown reason when we do
+  // in-memory/on-the-fly CTU (i.e when the compilation db is given) some
+  // parts of the triple in the loaded ASTContext can be unknown while the
+  // very same parts in the target ASTContext are known. Thus we check for
+  // the known parts only.
+  if (!hasEqualKnownFields(TripleTo, TripleFrom)) {
+    // TODO pass the SourceLocation of the CallExpression for more precise
+    // diagnostics
+    Context.getDiagnostics().Report(diag::err_ctu_incompat_triple)
+        << Unit->getMainFileName() << TripleTo.str() << TripleFrom.str();
+    ++NumTripleMismatch;
+    return llvm::make_error<IndexError>(index_error_code::triple_mismatch);
+  }
+  const auto& LangTo = Context.getLangOpts();
+  const auto& LangFrom = Unit->getASTContext().getLangOpts();
+  // FIXME: Currenty we do not support the across languages.
+  if (LangTo.CPlusPlus != LangFrom.CPlusPlus) {
+    ++NumLangMismatch;
+    return llvm::make_error<IndexError>(index_error_code::lang_mismatch);
+  }
 
   TranslationUnitDecl *TU = Unit->getASTContext().getTranslationUnitDecl();
   if (const FunctionDecl *ResultDecl =
